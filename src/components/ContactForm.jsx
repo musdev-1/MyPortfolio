be529cbea06966999fbd355343c7cc7878e3e47b
shahzebrehmankhattak <shahzebkhattak703@gmail.com>
2024-06-28 11:57:01 +0500

    spelling inn footer

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -35,7 +35,7 @@ const contactInfo = [
     color: " #075e54",
     title: "WhatsApp",
     description: "+92 03235254333",
-    link: "https://wa.me/+923235254333",
+    link: "https://wa.me/923235254333&text=Welcome",
   },
 ];
 
